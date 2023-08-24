e8247851af3c102db19f89a15a12644a462d3ca0
kibeomkim14 <kkb99705119@gmail.com>
2023-08-24 21:37:41 +0900

    option info file code

---
+++ src/option_info.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import json
+import requests
+
+from urls import BASE_OPTION_INFO_URL
+from paths import BASE_DATA_PATH
+
+
+if __name__ == '__main__':
+    
+    # all tradable option contract info 
+    option_info_dict  = json.loads(requests.get(BASE_OPTION_INFO_URL).text)
+    underlyings_info  = option_info_dict['optionContracts']
+    underlyings_info  = pd.DataFrame(underlyings_info)
+    all_contract_info = option_info_dict['optionSymbols']
+
+    option_contract_info = []
+    for i in range(len(all_contract_info)):
+        contract = option_info_dict['optionSymbols'][i]
+        _ = contract.pop('filters')
+        option_contract_info.append(contract)
+        
+    option_contract_info = pd.DataFrame(option_contract_info)
+    trade_info = option_contract_info.loc[:,['id', 'underlying', 'priceScale', 'quantityScale', 'quoteAsset']]
+    option_contract_info = option_contract_info.loc[:,['id', 'symbol', 'underlying', 'side', 'strikePrice', 'expiryDate', 'unit']]
+    option_contract_info['expiryDate'] = pd.to_datetime(option_contract_info['expiryDate'], unit='ms')
+
+    print(option_contract_info.shape)
+    print(underlyings_info.shape)
+    
+    print(BASE_DATA_PATH + 'underlyings.csv')
+    
+    # save option exchange information as csv
+    underlyings_info.to_csv(BASE_DATA_PATH + 'underlyings.csv')
+    option_contract_info.to_csv(BASE_DATA_PATH + 'contracts.csv')
+
---
+++ src/urls.py
@@ -0,0 +1,3 @@
+BASE_SPOT_DATA_URL   = "https://api.binance.com/api/v3/klines"
+BASE_OPTION_INFO_URL = "https://eapi.binance.com/eapi/v1/exchangeInfo"
+BASE_OPTION_DATA_URL = "https://eapi.binance.com/eapi/v1/klines"
